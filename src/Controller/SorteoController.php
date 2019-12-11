8059bd03b617982242f293cf4f52ca87d91be441
Alfonso Jiménez <yo@alfonsojimenez.net>
2019-12-11 09:46:38 +0100

    Apartado 2.3

---
+++ src/Controller/SorteoController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/SorteoController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SorteoController extends AbstractController
+{
+    public function numero()
+    {
+        $numero = random_int(0, 100);
+
+        return $this->render('sorteo/numero.html.twig', [
+            'numero' => $numero,
+        ]);
+    }
+}
