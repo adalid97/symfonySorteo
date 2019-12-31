ea54a23ff64e5b15f67a3e1f394db193c0291cd6
Alfonso Jiménez <yo@alfonsojimenez.net>
2019-12-31 19:22:38 +0100

    2.8.5

--- src/Controller/SorteoController.php
+++ src/Controller/SorteoController.php
@@ -101,4 +101,39 @@ class SorteoController extends AbstractController
     {
         return $this->render('sorteo/apuestaCreada.html.twig');
     }
+
+    public function verApuesta($id)
+    {
+        // Obtenemos el gestor de entidades de Doctrine
+        $entityManager = $this->getDoctrine()->getManager();
+
+        /* Obtenenemos el repositorio de Apuestas y 
+           buscamos en el usando la id de la apuesta */
+        $apuesta= $entityManager->getRepository(Apuesta::class)->find($id);
+
+        // Si la apuesta no existe lanzamos una excepción.
+        if (!$apuesta){
+            throw $this->createNotFoundException(
+                'No existe ninguna apuesta con id '.$id
+            );
+        }
+
+        /* Pasamos la apuesta a una plantilla que
+           se encargue de mostrar sus datos. */
+        return $this->render('sorteo/verApuesta.html.twig', array(
+            'apuesta' => $apuesta,
+        ));
+    }
+    public function listaApuestas()
+    {
+        // Obtenemos el gestor de entidades de Doctrine
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // obtenemos todas las apuestas
+        $apuestas= $entityManager->getRepository(Apuesta::class)->findAll();
+
+        return $this->render('sorteo/listaApuestas.html.twig', array(
+            'apuestas' => $apuestas,
+        ));
+    }
 }
